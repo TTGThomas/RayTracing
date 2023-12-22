2157e76fcef1051fdc693cac73dd4643b5f3c738
TTG <778977442@qq.com>
2023-12-22 21:41:13 +0800

    Improve Editor

--- TTG/TTG_app/Editor.cpp
+++ TTG/TTG_app/Editor.cpp
@@ -1,5 +1,9 @@
 #include "Editor.h"
 
+#include <glm/gtc/type_ptr.hpp>
+
+#include "shapes/Triangle.h"
+
 void Editor::BindScene(Scene* scene)
 {
 	m_activatedScene = scene;
@@ -41,24 +45,80 @@ void Editor::ShowWindow()
     if (ImGui::Button("Reset"))
         m_activatedRenderer->ResetFrameIndex();
 
-    
-    for (int i = 0; i < m_activatedScene->objects.size(); i++)
+    if (ImGui::CollapsingHeader("Objects"))
     {
-        ImGui::PushID(i);
-        HitType& type = m_activatedScene->objects[i]->m_type;
-        if (type == HitType::TRIANGLE)
+        ImGui::Indent();
+        for (int i = 0; i < m_activatedScene->objects.size(); i++)
         {
-            ImGui::SeparatorText("Triangle");
+            ImGui::PushID(i);
+            HitType& type = m_activatedScene->objects[i]->m_type;
+            if (type == HitType::TRIANGLE)
+            {
+                if (ImGui::CollapsingHeader("Triangle"))
+                {
+                    ImGui::Indent();
+                    ::Triangle* tri = dynamic_cast<::Triangle*>(m_activatedScene->objects[i].get());
+                    
+                    ImGui::DragFloat3("PointA", &tri->m_posA.x, 0.1f);
+                    ImGui::DragFloat3("PointB", &tri->m_posB.x, 0.1f);
+                    ImGui::DragFloat3("PointC", &tri->m_posC.x, 0.1f);
+
+                    if (ImGui::CollapsingHeader("Normals"))
+                    {
+                        ImGui::Indent();
+
+                        Shapes::Triangle shape;
+                        shape.Init(*tri);
+
+                        if (ImGui::Button("Compute Normal")) shape.ComputeNormal();
+                        if (ImGui::Button("Flip Normal")) shape.FlipNormal();
+                        if (ImGui::Button("Flip Sequence")) shape.FlipSequence();
+
+                        *tri = shape.GetData();
+                        ImGui::Unindent();
+                    }
+
+                    ImGui::DragInt("Material Index", &tri->m_matIndex, 0.1f, 0, (int)m_activatedScene->mat.size() - 1);
+                    ImGui::DragInt("Bsdf Index", &tri->m_bsdfIndex, 0.1f, 0, (int)m_activatedScene->bsdf.size() - 1);
+                    ImGui::Checkbox("Visible", &tri->m_visible);
+                    ImGui::Unindent();
+                }
+            }
+            else if (type == HitType::SPHERE)
+            {
+                if (ImGui::CollapsingHeader("Sphere"))
+                {
+                    ImGui::Indent();
+                    ::Sphere* sphere = dynamic_cast<::Sphere*>(m_activatedScene->objects[i].get());
+
+                    ImGui::DragFloat3("Position", &sphere->m_position.x, 0.1f);
+                    ImGui::DragFloat("Radius", &sphere->m_radius, 0.05f);
+
+                    ImGui::DragInt("Material Index", &sphere->m_matIndex, 0.1f, 0, (int)m_activatedScene->mat.size() - 1);
+                    ImGui::DragInt("Bsdf Index", &sphere->m_bsdfIndex, 0.1f, 0, (int)m_activatedScene->bsdf.size() - 1);
+                    ImGui::Checkbox("Visible", &sphere->m_visible);
+                    ImGui::Unindent();
+                }
+            }
+            else if (type == HitType::PLANE)
+            {
+                if (ImGui::CollapsingHeader("Plane"))
+                {
+                    ImGui::Indent();
+                    ::Plane* plane = dynamic_cast<::Plane*>(m_activatedScene->objects[i].get());
+                    
+                    ImGui::DragFloat3("Position", &plane->m_position.x, 0.1f);
+                    ImGui::DragFloat3("Normal", &plane->m_normal.x, 0.1f);
+
+                    ImGui::DragInt("Material Index", &plane->m_matIndex, 0.1f, 0, (int)m_activatedScene->mat.size() - 1);
+                    ImGui::DragInt("Bsdf Index", &plane->m_bsdfIndex, 0.1f, 0, (int)m_activatedScene->bsdf.size() - 1);
+                    ImGui::Checkbox("Visible", &plane->m_visible);
+                    ImGui::Unindent();
+                }
+            }
+            ImGui::PopID();
         }
-        else if (type == HitType::SPHERE)
-        {
-            ImGui::SeparatorText("Sphere");
-        }
-        else if (type == HitType::PLANE)
-        {
-            ImGui::SeparatorText("Plane");
-        }
-        ImGui::PopID();
+        ImGui::Unindent();
     }
 
     ImGui::End();
